fb9bcca6ec197fb2034a5a374684e71f5ddee906
unknown <jhonatasenac15@gmail.com>
2024-08-09 13:30:02 -0300

    primeira etapa

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  process.env.TZ = '-03:00';
-
-  app.useGlobalPipes(new ValidationPipe);
-
+  process.env.TZ = '03:00';
   app.enableCors();
-  
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
 bootstrap();
